8a472353ca72909266bf6ee6f76a4842f7f982a7
subtle-i <innoway.subtle@gmail.com>
2022-07-24 22:50:28 +0900

    2nd commit

---
+++ src/components/ActionButton.jsx
@@ -0,0 +1,9 @@
+// ActionButton.jsx
+
+export const ActionButton = ({text, action}) => {
+    return (
+      <>
+        <button type="button" onClick={action}>{text}</button>
+      </>
+    );
+  };
